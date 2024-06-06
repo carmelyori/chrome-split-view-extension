a3bdd9b019818d825b37ea36ca79f83824fcc7e9
Ori Carmely <130679187+carmelyori@users.noreply.github.com>
2024-06-06 13:54:59 +0300

    Add popup.js file

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.getElementById('urlForm').addEventListener('submit, function(e) {
+  e.preventDefault();
+  const url1 = document.getElementById('url1').value;
+  const url2 = document.getElementById('url2').value;
+  chrome.tabs.create({ url: chrome.runtime.getURL('split_view.html') }, function(tab) {
+    chrome.tabs.executeScript(tab.id, {
+      code: `window.url1 = "${url1}"; window.url2 = "${url2}";`
+    });
+  });
+});
